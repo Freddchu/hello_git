5d0ed3c2ae8e1e1f00be98c833fd8795db045971
Freddy Churata Mayta <atyamsleep@gmail.com>
2026-05-24 19:41:35 -0400

    Improve homepage design

--- js/app.js
+++ js/app.js
@@ -13,7 +13,28 @@ $(function(){
         $(this).addClass("activa");
     });
 
-    setTimeout(function(){
-        $("#avance").animate({ width:"75%" }, 900);
-    }, 400);
+    $(".menu a").on("click", function(){
+        $(".menu a").removeClass("activo");
+        $(this).addClass("activo");
+    });
+
+    $("#formulario-contacto").on("submit", function(evento){
+        evento.preventDefault();
+        $("#respuesta-formulario").hide().text("Mensaje preparado. Puedes conectar este formulario despues.").fadeIn(250);
+        this.reset();
+    });
+
+    let porcentaje = 0;
+    const final = 82;
+    const intervalo = setInterval(function(){
+        porcentaje += 2;
+        $("#porcentaje").text(porcentaje + "%");
+        $("#avance").css("width", porcentaje + "%");
+
+        if(porcentaje >= final){
+            clearInterval(intervalo);
+            $("#porcentaje").text(final + "%");
+            $("#avance").css("width", final + "%");
+        }
+    }, 22);
 });
